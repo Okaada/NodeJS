ebdcb09fe4cd3f3e66645223a372539ebf6ac665
Okaada <ravelokada@gmail.com>
2020-05-19 19:30:15 -0300

    First Commit

---
+++ src/controllers/authController.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        if (User.findOne({email})) 
+        return res.status(400).send({ error: 'This email already exists' });
+
+        const user = await User.create(req.body)
+
+        user.password = undefined;
+
+        return res.send({ user })
+    } catch (err) {
+        return res.status(400).send({ error: 'Failed to register' });
+
+    }
+});
+
+module.exports = app => app.use('/auth', router);
